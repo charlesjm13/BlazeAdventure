490a717ecf628fc767ee77a1b97c751f39f876f5
Charles Mennerick <charlesjm13@gmail.com>
2021-01-22 13:42:08 -0600

    Added character movement and camera tracking

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.x, this.transform.position.z);
+        
+    }
+}
---
+++ Assets/Scripts/HorizontalMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour
+{
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKey("a"))
+        {
+            position.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            position.x += speed * Time.deltaTime;
+        }
+
+        transform.position = position;
+    }
+}
