fa5d4ae15e984a9bb067c664374df502327cba19
Charles Mennerick <charlesjm13@gmail.com>
2021-01-22 14:26:05 -0600

    more additions to basic movement and game level

--- Assets/Scripts/HorizontalMovement.cs
+++ Assets/Scripts/HorizontalMovement.cs
@@ -6,11 +6,6 @@ public class HorizontalMovement : MonoBehaviour
 {
     public float speed = 10;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
