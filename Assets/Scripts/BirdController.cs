07c83e28bc421e497191b50bbdd905dd56dd47c8
Charles Mennerick <charlesjm13@gmail.com>
2021-01-28 14:28:56 -0600

    Added bird spawn and altered collider size

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class BirdController : MonoBehaviour
 {   
     public float MovementSpeed = -8;
+    public Vector3 birdLocation;
     // Start is called before the first frame update
     void Start()
     {
-        
+       this.transform.position = birdLocation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(MovementSpeed,0,0) * Time.deltaTime; 
+        if (this.transform.position.x < -10)
+        {
+            birdLocation.y += 1;
+            if (birdLocation.y > 4)
+            {
+                birdLocation.y = -2;
+            }
+            this.transform.position = birdLocation;
+        }
+        else
+        {
+            this.transform.position += new Vector3(MovementSpeed, 0, 0) * Time.deltaTime;
+        }
     }
 }
