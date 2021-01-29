ac622456f5d683a0ec2ff3dc4dc16e8bfcd0355a
Phan Anh Tuan Nguyen <tnguyenphananh@knox.edu>
2021-01-29 15:34:10 -0600

    added animation to the health bar if we touch the spike or enemy

---
+++ Assets/Scripts/PlayerHP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour
+{
+
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag ("Enemy") || coll.gameObject.CompareTag("Spikes"))
+        {
+            TakeDamage(4);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+}
