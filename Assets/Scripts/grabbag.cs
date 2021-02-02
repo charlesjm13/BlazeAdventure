a8209c7e89b3c98fae9e1ebbb90ab253d5b32279
Phan Anh Tuan Nguyen <tnguyenphananh@knox.edu>
2021-02-02 07:40:37 -0600

    added collectibles counter still trying to make it reset if the character die

---
+++ Assets/Scripts/grabbag.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabbag : MonoBehaviour
+{
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        ScoreManager.bagAmount += 1;
+        Destroy(gameObject);
+    }
+    
+}
