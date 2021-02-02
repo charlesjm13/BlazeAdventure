9fc481e06a71817092ebe29bd02b6c6f0d450636
khuele11 <ktle@knox.edu>
2021-02-02 14:17:52 -0600

    Added Pause button and functionally

---
+++ Assets/Scripts/PauseButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseButton : MonoBehaviour
+{
+    public void ButtonPause()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
