ddd00d7f3aa626653320e7a7556ca4773d5fe670
Justin Cooper <>
2021-02-01 18:03:18 -0700

    Added enemies knight and pirate with animation, update jump zone on bird

---
+++ Assets/Scripts/KnightControlller.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KnightControlller : MonoBehaviour
+{
+    [SerializeField] private LayerMask playerLayerMask;
+    public int MovementSpeed = -8;
+    public int tempSpeed;
+    private Vector2 rayDirection = new Vector2(0,0);
+    public int size = 12;
+    private Vector3 theScale;
+    private BoxCollider2D boxCollider2D;
+    public Animator animator;
+    private bool movingLeft = true;
+    private bool slashDone = true;
+    void Start()
+    {
+        theScale = transform.localScale;
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+        tempSpeed = MovementSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position += new Vector3(MovementSpeed,0,0) * Time.deltaTime; //movement and movement animation
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision){
+            
+            StartCoroutine(TurnWait());
+            
+    }
+
+    void FixedUpdate(){
+        
+        if(MovementSpeed < 0){
+            movingLeft = true;
+        }
+        if(MovementSpeed > 0){
+            movingLeft = false;
+        }
+        if(movingLeft){
+            Debug.DrawRay(boxCollider2D.bounds.center, Vector2.left * 2, Color.green);
+            rayDirection = Vector2.left;
+        }
+
+        if(!movingLeft){
+            Debug.DrawRay(boxCollider2D.bounds.center, Vector2.right * 2, Color.green);
+            rayDirection = Vector2.right;
+        }
+
+        if (Physics2D.Raycast(boxCollider2D.bounds.center, rayDirection ,2, playerLayerMask)){
+            
+            if(slashDone){
+            slashDone = false;
+            StartCoroutine(KillWait());
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collison){
+        
+    }
+    IEnumerator TurnWait(){
+    //Debug.Log("Died Wait");
+            MovementSpeed = -MovementSpeed; 
+            tempSpeed = MovementSpeed;
+            yield return new WaitForSeconds(0.225f);
+            size = -size;
+            theScale.x = size;
+            transform.localScale = theScale;
+            //Debug.Log("Hit");
+            
+   
+    }
+    IEnumerator KillWait(){
+        MovementSpeed = 0;
+        animator.Play("Knight Slash",  -1, 0f);
+        yield return new WaitForSeconds(0.75f);
+        if (Physics2D.Raycast(boxCollider2D.bounds.center, rayDirection ,2, playerLayerMask)){
+             Time.timeScale = 0;
+             yield return new WaitForSecondsRealtime(1);
+             SceneManager.LoadScene("FirstLevel");
+             Time.timeScale = 1;
+        }
+        MovementSpeed = tempSpeed;
+        slashDone = true;
+    }
+}
---
+++ Assets/Scripts/PirateController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private LayerMask playerLayerMask;
+    private BoxCollider2D boxCollider2D;
+    public Transform ShootPoint;
+    public GameObject MusketBallPrefab;
+    public Animator animator;
+    private bool shotDone= true;
+    void Start()
+    {
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Debug.DrawRay(boxCollider2D.bounds.center, Vector2.left * 15, Color.green);
+        if (Physics2D.Raycast(boxCollider2D.bounds.center, Vector2.left ,15, playerLayerMask)){
+
+           if(shotDone){
+            shotDone = false;
+            StartCoroutine(ShotWait());
+            }
+
+        }
+    }
+    IEnumerator ShotWait(){
+            animator.SetBool("Shot", false);
+            animator.Play("Pirate Shoot",  -1, 0f);
+            yield return new WaitForSeconds(0.5f);
+            animator.SetBool("Shot", true);
+            Instantiate(MusketBallPrefab, ShootPoint.position, ShootPoint.rotation);
+            yield return new WaitForSeconds(5);
+            shotDone = true;
+    }
+}
