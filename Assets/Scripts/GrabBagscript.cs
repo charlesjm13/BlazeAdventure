835fbcb2b30ce5944a576087c78f85f03ce5ac4e
Phan Anh Tuan Nguyen <tnguyenphananh@knox.edu>
2021-02-02 02:33:42 -0600

    add counter still working on reset the counter when the character die

---
+++ Assets/Scripts/GrabBagscript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabBagscript : MonoBehaviour
+{
+
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        BagNumber.bagAmount += 1;
+        Destroy(gameObject);
+    }
+
+}
