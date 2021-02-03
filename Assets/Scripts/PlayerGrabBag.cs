eb0902bb83b505ee41959a44a5b16a9c29fba6fb
Phan Anh Tuan Nguyen <tnguyenphananh@knox.edu>
2021-02-02 20:35:08 -0600

    collectible counter reset and change the location of pause button

---
+++ Assets/Scripts/PlayerGrabBag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrabBag : MonoBehaviour
+{
+    private int StartingBag;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreManager.bagAmount = StartingBag;
+    }
+
+    
+}
