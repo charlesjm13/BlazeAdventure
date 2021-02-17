87c2f60f6b319045e090e8874b513690eccbc6eb
Phan Anh Tuan Nguyen <tnguyenphananh@knox.edu>
2021-02-16 23:09:10 -0600

    added life counter and when you collect 4 grab bag you get 1 extra life

---
+++ Assets/LifeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeManager : MonoBehaviour
+{
+    public static LifeManager instance;
+    Text text;
+    public static int lifeAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        text.text = lifeAmount.ToString();
+    }
+}
