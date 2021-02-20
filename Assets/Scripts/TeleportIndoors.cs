19a182a54000468e298fd0c9592feada62fe3bbe
Charles Mennerick <charlesjm13@gmail.com>
2021-02-20 10:34:46 -0600

    added indoor ground and teleport between indoor and outdoor

---
+++ Assets/Scripts/TeleportIndoors.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportIndoors : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 indoorLocation;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        this.player.transform.position = new Vector3(indoorLocation.x, indoorLocation.y, indoorLocation.z);
+    }
+}
