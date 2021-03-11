9ac65af88230b009ec60a2b42b7be27a4fc18dec
Charles Mennerick <charlesjm13@gmail.com>
2021-03-11 10:51:45 -0600

    Added reverse pirate and more indoor obstacles

---
+++ Assets/Scripts/ShootFlipped.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootFlipped : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.velocity = Vector2.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
